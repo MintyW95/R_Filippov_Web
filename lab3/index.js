21c0f78112203124920943699099ac660967b135
Филиппов Роман Александрович <romkafilippov8@gmail.com>
2026-03-09 10:14:37 +0300

    съехали чутчут табы

--- lab3/index.js
+++ lab3/index.js
@@ -9,13 +9,13 @@ function playSlot()
     {
         let bet = prompt("У вас " + chips + " фишек. Сколько ставите?");
         if (bet === null) 
-	{
+		{
             alert("Приходите ещё!");
             break;
         }
         bet = Number(bet);
         if (isNaN(bet) || bet > chips || bet <= 0) 
-	{
+		{
             alert("ЧОТА НЕ ТО ВВЁЛ");
             continue;
         }
@@ -28,7 +28,7 @@ function playSlot()
         alert(`${slot1} | ${slot2} | ${slot3}`);
         let win = bet;
         if (slot1 === slot2 && slot2 === slot3) 
-	{
+		{
             switch(slot1) 
 	    {
                 case "🍒": win = win * 25; prompt("МЕГА ВЫИГРЫШ!!! ВЫ НА ТЕЛЕКЕ!!! хотите что-то передать маме?"); break; 
@@ -41,23 +41,23 @@ function playSlot()
             alert("+" + (win-bet) + " фишек!");
             wincount += 1;
             if (wincount == 3) 
-	    {
+	   		{
                 alert("I'm sorry, you just win too much.");
                 alert("[ВАМ ЗАПРЕТИЛИ ИГРАТЬ В ЭТОМ КАЗИНО]");
-                break; // Также можно завершить игру здесь  
+                break; 
             }
-        } 
-	else if (slot1 === "🍒" || slot2 === "🍒" || slot3 === "🍒") 
-	{
-            win = win * 2;
-            chips += win;
-            alert("Утешительный приз: +" + (win-bet) + " фишек!");
-        } 
-	else 
-	{
-            alert("Ты, наверное, думаешь, что тебе выпало 18 карат невезения? Да нет, просто игра попалась нечестная");
-        }
-        if (!confirm("Хе-хе, повторим?")) break;
+    	} 
+		else if (slot1 === "🍒" || slot2 === "🍒" || slot3 === "🍒") 
+		{
+    	        win = win * 2;
+    	        chips += win;
+    	        alert("Утешительный приз: +" + (win-bet) + " фишек!");
+    	} 
+		else 
+		{
+    	    alert("Ты, наверное, думаешь, что тебе выпало 18 карат невезения? Да нет, просто игра попалась нечестная");
+		}
+    	if (!confirm("Хе-хе, повторим?")) break;
     }
     alert("Последний дэп поставил жирную точку в этой истории. Я снял руку с рычага — всё было кончено. Мой баланс был " + chips + " фишек...");
 }
